531793ef9313f6334e77dfc818f5c7fa37f64526
Yorick <girard.yorick@gmail.com>
2015-06-10 15:13:33 +0200

    Ajout de la fonctionnalité de connexion et de déconnexion

--- src/jsf2/ConnectUtilisateur.java
+++ src/jsf2/ConnectUtilisateur.java
@@ -2,6 +2,7 @@ package jsf2;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.persistence.NoResultException;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
@@ -13,19 +14,51 @@ import utils.IUtilisateurDAO;
 @ManagedBean
 public class ConnectUtilisateur {
 	
+	private String email;
+	private String password;
+	
 	private Utilisateur utilisateur;
 	private IUtilisateurDAO utilisateurDAO;
 	
-	public void setUtilisateurDAO(IUtilisateurDAO utilisateurDAO){ this.utilisateurDAO = utilisateurDAO; }
-	public IUtilisateurDAO getUtilisateurDAO(){ return utilisateurDAO; }
+	public void setUtilisateurDAO(IUtilisateurDAO utilisateurDAO){ 
+		this.utilisateurDAO = utilisateurDAO; 
+	}
+	public IUtilisateurDAO getUtilisateurDAO(){
+		return utilisateurDAO; 
+	}
 	
-	public String connect(String email, String password)
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	public String connect()
 	{
-		
-		utilisateur = getUtilisateurDAO().findByEmailAndPassword(email, password);
-		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-		
+		try{
+			utilisateur = getUtilisateurDAO().findByEmailAndPassword(email, password);
+		}
+		catch (NoResultException e) {
+			return "index";
+		}
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
 		session.setAttribute("utilisateur", utilisateur);
+		
+		return "index";
+	}
+	
+	public String disconnect()
+	{
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+		session.removeAttribute("utilisateur");
 		
 		return "index";
 	}
